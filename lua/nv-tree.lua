5ad6e6087aff91a574024ea5f10e68753582a227
Isak Simonnes Mella <IM@dokflyt.no>
2021-12-08 13:24:57 +0100

    fix change to nvim-tree config

--- lua/nv-tree.lua
+++ lua/nv-tree.lua
@@ -3,7 +3,6 @@ opts = { noremap = true, silent = false }
 
 vim.o.termguicolors = true
 
-vim.g.nvim_tree_gitignore = 1
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_highlight_opened_files = 1
 vim.cmd([[
@@ -30,6 +29,10 @@ require("nvim-tree").setup({
 			error = "",
 		},
 	},
+  git = {
+    enable = true,
+    ignore = true,
+  },
 	update_focused_file = {
 		enable = true,
 		update_cwd = true,
