ee2581360a2f51b19d1307564ac7123d3bee6d3b
Isaaki <xisaacnewton1x@gmail.com>
2023-01-06 03:35:58 +0100

    changed conflicting keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Find files [Telescope]" })
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Git files [Telescope]" })
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>of', builtin.find_files, { desc = "Find files [Telescope]" })
+vim.keymap.set('n', '<leader>og', builtin.git_files, { desc = "Git files [Telescope]" })
+vim.keymap.set('n', '<leader>os', function()
     builtin.grep_string({ search = vim.fn.input("grep > ") });
 end, { desc = "Project search [Telescope]" })
