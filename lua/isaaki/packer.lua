8fa6029769405624e7f479b916b9d8009aefd512
Isaaki <xisaacnewton1x@gmail.com>
2023-01-08 04:04:44 +0100

    add visual-multi plugin

--- lua/isaaki/packer.lua
+++ lua/isaaki/packer.lua
@@ -89,4 +89,6 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+
+    use { "mg979/vim-visual-multi", branch = "master" }
 end)
