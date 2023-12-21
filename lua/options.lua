dc63eb7cef962c4c76e4f4678d6d57769dff2806
isaaki <xisaacnewton1x@gmail.com>
2023-12-21 12:43:11 +0100

    refactor config and add some plugins

--- lua/options.lua
+++ lua/options.lua
@@ -13,6 +13,7 @@ vim.opt.incsearch = true
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 
 vim.opt.smartindent = true
---
+++ lua/plugins/ufo.lua
@@ -0,0 +1,35 @@
+return {
+  "kevinhwang91/nvim-ufo",
+  -- dependencies = { "kevinhwang91/promise-async", "nvim-treesitter/nvim-treesitter" },
+  dependencies = {
+    "kevinhwang91/promise-async",
+    {
+      "luukvbaal/statuscol.nvim",
+      config = function()
+        local builtin = require("statuscol.builtin")
+        require("statuscol").setup({
+          relculright = true,
+          segments = {
+            { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+            { text = { "%s" }, click = "v:lua.ScSa" },
+            { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+          },
+        })
+      end,
+    },
+  },
+  config = function()
+    vim.o.foldcolumn = "1" -- '0' is not bad
+    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+    vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
+    require('ufo').setup({
+      open_fold_hl_timeout = 100,
+      provider_selector = function(bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+      end
+    })
+  end,
+}
