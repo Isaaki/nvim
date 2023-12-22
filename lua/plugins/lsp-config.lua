73fff298dd9c130615b74175ab539ec5426f6a2f
isaaki <xisaacnewton1x@gmail.com>
2023-12-22 06:28:24 +0100

    add .luarc.json for lua LSP support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,47 +10,39 @@ return {
     dependencies = "williamboman/mason.nvim",
     config = function()
       require("mason-lspconfig").setup {
-        --ensure_installed = { "lua_ls" },
+        ensure_installed = { "lua_ls" },
       }
     end
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require'lspconfig'.lua_ls.setup {
-        on_init = function(client)
-          local path = client.workspace_folders[1].name
-          if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
-            client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-              Lua = {
-                runtime = {
-                  -- Tell the language server which version of Lua you're using
-                  -- (most likely LuaJIT in the case of Neovim)
-                  version = 'LuaJIT'
-                },
-                -- Make the server aware of Neovim runtime files
-                workspace = {
-                  checkThirdParty = false,
-                  library = {
-                    vim.env.VIMRUNTIME
-                    -- "${3rd}/luv/library"
-                    -- "${3rd}/busted/library",
-                  }
-                  -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                  -- library = vim.api.nvim_get_runtime_file("", true)
-                }
-              }
-            })
+      vim.lsp.set_log_level("debug")
 
-            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-          end
-          return true
-        end
-      }
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover symbol | LSP" })
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "GoTo definition | LSP" })
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "GoTo declaration | LSP" })
-      vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, { desc = "Code action | LSP" })
-    end
+      local lspconfig = require('lspconfig')
+      lspconfig.lua_ls.setup {}
+      lspconfig.tsserver.setup {}
+
+      -- Use LspAttach autocommand to only map the following keys
+      -- after the language server attaches to the current buffer
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+          -- Buffer local mappings.
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = ev.buf, desc = "GoTo definition | LSP" })
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = ev.buf, desc = "GoTo declaration | LSP" })
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = ev.buf, desc = "Hover symbol | LSP" })
+          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action,
+            { buffer = ev.buf, desc = "Code action | LSP" })
+          vim.keymap.set('n', '<leader>cf', function()
+            vim.lsp.buf.format { async = true }
+          end, { buffer = ev.buf, desc = "Format | LSP" })
+        end,
+      })
+    end,
   },
 }
